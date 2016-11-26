f0d7cc58f2974ed441a6a23a1db06d28509e42db
--get-all <vanven08ctt3@gmail.com>
2016-11-26 11:27:45 +0700

    renew auth

--- src/app/models/user.rb
+++ src/app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
   has_many :friend_requests, class_name:"Friendship", foreign_key: :friend_id
 
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    @user = where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
       user.uid      = auth.uid
       user.name     = auth.info.name
@@ -25,6 +25,10 @@ class User < ApplicationRecord
       user.encrypted_password = ""
       user.save(:validate => false)
     end
+    @user.oauth_token = auth.credentials.token
+    @user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+    @user.save
+    return @user
   end
 
   def unconfirmed_friend_requests
